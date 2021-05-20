18cc729601b3e10a54120c77fdc78280b245d2f0
aqilusman45 <aqilusman45@gmail.com>
2021-05-20 11:34:08 +0500

    change return data type to object for postHello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,8 +15,12 @@ export class AppController {
   }
 
   @Post()
-  postHello(): string {
-    return this.appService.postHello();
+  postHello(): {
+    data: string;
+  } {
+    return {
+      data: "some data",
+    };
   }
 
   @Get()
